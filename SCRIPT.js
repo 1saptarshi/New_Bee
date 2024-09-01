c4ce6838db3e711edaf275b1d4f6e15383ef3814
saptarshi chowdhury <142312774+1saptarshi@users.noreply.github.com>
2024-09-01 18:27:50 +0530

    Add files via upload

---
+++ SCRIPT.js
@@ -0,0 +1,44 @@
+// Theme Toggle Logic
+const themeToggle = document.getElementById('theme-toggle');
+const themeIcon = document.getElementById('theme-icon');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    
+    if (body.classList.contains('dark-mode')) {
+        themeIcon.classList.replace('fa-sun', 'fa-moon');
+    } else {
+        themeIcon.classList.replace('fa-moon', 'fa-sun');
+    }
+});
+
+// Scroll effect
+const scrollElements = document.querySelectorAll('.scroll-effect');
+
+const elementInView = (el, dividend = 1) => {
+    const elementTop = el.getBoundingClientRect().top;
+    return elementTop <= (window.innerHeight || document.documentElement.clientHeight) / dividend;
+};
+
+const displayScrollElement = (element) => {
+    element.classList.add('visible');
+};
+
+const hideScrollElement = (element) => {
+    element.classList.remove('visible');
+};
+
+const handleScrollAnimation = () => {
+    scrollElements.forEach((el) => {
+        if (elementInView(el, 1.25)) {
+            displayScrollElement(el);
+        } else {
+            hideScrollElement(el);
+        }
+    });
+};
+
+window.addEventListener('scroll', () => {
+    handleScrollAnimation();
+});
